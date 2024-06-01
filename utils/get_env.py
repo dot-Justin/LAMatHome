312264cc0357a9a4a50edd79bff8b72f595adbc7
pnivek <88808109+pnivek@users.noreply.github.com>
2024-06-01 03:17:27 -0400

    Adding Journal Module (#12)
    * added journal module to fetch user journals via API
    
    * remove uneeded strings from merge conflict

--- utils/get_env.py
+++ utils/get_env.py
@@ -5,10 +5,11 @@ load_dotenv()
 
 RH_EMAIL = os.getenv("RH_EMAIL")
 RH_PASS = os.getenv("RH_PASS")
+RH_ACCESS_TOKEN = os.getenv("RH_ACCESS_TOKEN")
 FB_EMAIL = os.getenv("FB_EMAIL")
 FB_PASS = os.getenv("FB_PASS")
 DC_EMAIL = os.getenv("DC_EMAIL")
 DC_PASS = os.getenv("DC_PASS")
 
-if not RH_EMAIL or not RH_PASS or not FB_EMAIL or not FB_PASS or not DC_EMAIL or not DC_PASS:
+if ((not RH_EMAIL or not RH_PASS) and not RH_ACCESS_TOKEN) or not FB_EMAIL or not FB_PASS or not DC_EMAIL or not DC_PASS:
     raise ValueError("Failed to load environment variables. Please check the .env file.")

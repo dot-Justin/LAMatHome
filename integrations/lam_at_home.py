7f596aa621a2936b7767fc01d990197b319bf0c6
Kevin <88808109+pnivek@users.noreply.github.com>
2024-06-15 19:56:02 -0400

    add support for saving entry data, fixed bugs, some refactors (#56)
    - new function for LAH int, can save images/resources
    - small fixes and refactoring

--- integrations/lam_at_home.py
+++ integrations/lam_at_home.py
@@ -1,8 +1,39 @@
+import os
 import sys
-from utils.splash_screen import colored_splash_goodbye
+import logging
+from utils import splash_screen, config, journal
 
 
-# Function to sys.exit to terminate LAH
-def terminate():
-    print(colored_splash_goodbye)
+def save(entry: journal.Entry) -> None:
+    '''
+    Save the given entry's resources to disk.
+    '''
+    if entry:
+        # Save resources if enabled in config
+        if config.config['lamathomesave_isenabled']:
+            # resolve save path
+            save_path = config.config['lamathomesave_path']
+            save_path = os.path.expanduser(save_path)
+            save_path = os.path.expandvars(save_path)
+            
+            # Ensure the directory exists
+            if not os.path.exists(save_path):
+                try:
+                    os.makedirs(save_path)
+                except OSError as error:
+                    logging.error(f"Error creating directory: {error}")
+                    # Fallback to cache directory if creation fails
+                    save_path = os.path.expandvars(config.config['cache_dir'])
+            
+            entry.save_resources(save_path)
+
+
+def terminate() -> None:
+    '''
+    Terminate the LAMatHome application.
+    '''
+    if not config.config["lamathometerminate_isenabled"]:
+        logging.error("LAMatHome termination is not enabled.")
+        return
+    print(splash_screen.colored_splash_goodbye)
     sys.exit()
